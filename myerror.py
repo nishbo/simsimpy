0d408ed18c9f085f268c553111a89f3f846234a9
Anton Sobinov (mac) <nishbo@yandex.ru>
2014-03-04 15:06:46 +0400

    Hodgkin-Huxley neuron module
    Module with a Neuron() class that represents a a basic Hodgkin-Huxley
    neuron as described in the original paper. For more info use help.
    
    myerror.py contains a simple error class for me to use.

---
+++ myerror.py
@@ -0,0 +1,15 @@
+class Error(Exception):
+    """Handles errors that occur in this class."""
+
+    def __init__(self, msg, value):
+        """Inits an exception with a message and wrong value.
+
+        Args:
+            msg: String with description of what happened.
+            value: Value that supposedly was wrongly provided.
+        """
+        self.msg = msg
+        self.value = value
+
+    def __str__(self):
+        return repr(self.msg + 'Value: ' + str(self.value))
